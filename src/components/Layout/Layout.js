60ba7f3ea9f99172c106ad36dc0202610a634dbd
Pawan Kumar <kumar16.pawan@gmail.com>
2020-06-23 10:53:18 +0530

    Rendering Burger Ingredients

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Aux from '../../hoc/Aux'
+import classes from './Layout.css'
 
 export default function Layout(props) {
     return (
         <Aux>
             <div>
                 Toolbar,Sidebar, Backdrop
-           </div>
-            <main>
+           </div >
+            <main className={classes.Content}>
                 {props.children}
             </main>
         </Aux>
